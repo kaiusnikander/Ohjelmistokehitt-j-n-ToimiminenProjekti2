851b8eb89ef2bec55c6d1dda183b7985c052ba2d
jimieloranta <82642208+jimieloranta@users.noreply.github.com>
2022-08-17 14:17:30 +0300

    Add files via upload

---
+++ yhteys.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "je0617";  // Käyttäjän nimi ja samalla tietokanta-alueen nimi; korvaa omallasi
+$password = "JimiEloranta";     // Käyttäjän salasana; korvaa omallasi
+try {
+       $yhteys = new PDO("mysql:host=$servername;dbname=$username", $username, $password);
+       $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Yhteys muodostettu<br>";
+    }
+catch(PDOException $e)
+    {
+    echo "Ei yhteyttä tietokantaan!<br> " . $e->getMessage();
+    }
+?>
