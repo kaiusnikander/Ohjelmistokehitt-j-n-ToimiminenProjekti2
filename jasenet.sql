10f81435595da0b132af23f5bd24c1ed25b94b32
jimieloranta <82642208+jimieloranta@users.noreply.github.com>
2022-09-28 15:24:20 +0300

    Add files via upload

---
+++ jasenet.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 28.09.2022 klo 14:23
+-- Palvelimen versio: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `je0617`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Rakenne taululle `jasenet`
+--
+
+CREATE TABLE `jasenet` (
+  `etunimi` text NOT NULL,
+  `sukunimi` text NOT NULL,
+  `sahkoposti` text NOT NULL,
+  `koulutalo` text NOT NULL,
+  `viesti` text NOT NULL,
+  `jaosto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vedos taulusta `jasenet`
+--
+
+INSERT INTO `jasenet` (`Id`, `etunimi`, `sukunimi`, `sahkoposti`, `koulutalo`, `viesti`, `jaosto`) VALUES
+(0, 'Jimi', 'Eloranta', 'je0617@edu.turku.fi', 'Juhannuskukkula', 'ncknicwuiwewnr', 'harrastejaosto'),
+(0, 'Jimi', 'Eloranta', 'je0617@edu.turku.fi', 'Juhannuskukkula', 'ncknicwuiwewnr', 'harrastejaosto');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
